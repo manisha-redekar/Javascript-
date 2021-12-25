f2555c75f58e33c359cc9bfad0fa9c4d198dd4a3
manisha-redekar <95485270+manisha-redekar@users.noreply.github.com>
2021-12-25 22:32:03 +0530

    Add files via upload

---
+++ area of circle.js
@@ -0,0 +1,3 @@
+var circleRadius = parseInt(prompt("Please enter the radius of circle:"));
+var areaOfCircle = Math.PI * (circleRadius * circleRadius);
+console.log("Area of circle is: " + areaOfCircle);
