0999edbac9a11c859479bb45de2350cb70f6f5f2
manisha-redekar <95485270+manisha-redekar@users.noreply.github.com>
2021-12-25 22:24:31 +0530

    Add files via upload

---
+++ odd and even.js
@@ -0,0 +1,8 @@
+
+const number = prompt("Enter a to check odd or even number: ");
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
